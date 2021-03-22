b68f3a4b04ae5a8eb08c48075691421fe87ad921
Westmist <mingxuannull@gmail.com>
2021-03-22 17:43:18 +0800

    路由测试 & Echarts引入

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+// 自定义配置，参考 ./default/setting.config.js，需要自定义的属性在这里配置即可
+module.exports = {
+  multiPage: true,
+  animate: {
+    name: "back",
+    direction: "left",
+  },
+  //隐藏设置抽屉，true:隐藏，false:不隐藏
+  hideSetting: true,
+};
---
+++ src/pages/test/Test.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="test">
+    <div id="echarts"></div>
+  </div>
+</template>
+<script>
+import Chart from "echarts";
+export default {
+  name: "Test",
+  data() {
+    return {
+      echarts: null,
+    };
+  },
+  methods: {
+    initEcharts() {
+      let option = {
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: "line",
+          },
+        ],
+      };
+      this.echarts.setOption(option);
+    },
+  },
+  mounted() {
+    this.echarts = Chart.init(document.getElementById("echarts"));
+    this.initEcharts();
+  },
+};
+</script>
+<style scoped>
+#echarts {
+  width: 90%;
+  /*  高度不能为100% */
+  height: 600px;
+}
+</style>
